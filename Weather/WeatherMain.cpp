f7c517e7287981aae1807368e0227ec25908a987
Min Zhu <mzhu92@uwo.ca>
2018-11-08 23:33:59 -0500

    test cases

--- Weather/WeatherMain.cpp
+++ Weather/WeatherMain.cpp
@@ -6,8 +6,27 @@ int main()
 {
 	Weather weather;
     //string output = weather.getWeather("London,CA");
-	vector<string> output = weather.getWeather("London,CA");
 
-    cout << "OUTPUT" << endl;
-    cout << "Temp:" << output[0] << "  condition:" << output[1] << endl;
+    cout << "Test 1: London, CA" << endl;
+	vector<string> output = weather.getWeather("London,CA");
+    cout << "London,CA" << "  Temp:" << output[0] << "  condition:" << output[1] << endl;
+
+
+    cout << "Test 2: London, GB" << endl;
+	output = weather.getWeather("London,GB");
+    cout  << "London,GB" << "  Temp:" << output[0] << "  condition:" << output[1] << endl;
+
+
+    cout << "Test 3: Shanghai" << endl;
+	output = weather.getWeather("Shanghai");
+    cout  << "Shanghai" << "  Temp:" << output[0] << "  condition:" << output[1] << endl;
+
+
+    cout << "Test 4: Nanjing" << endl;
+	output = weather.getWeather("Nanjing");
+    cout  << "Nanjing" << "  Temp:" << output[0] << "  condition:" << output[1] << endl;
+
+        cout << "Test 5: Shenyang" << endl;
+	output = weather.getWeather("Shenyang");
+    cout  << "Shenyang" << "  Temp:" << output[0] << "  condition:" << output[1] << endl;
 }
