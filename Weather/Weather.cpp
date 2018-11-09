005a8cccb0e9424af1b330d9e118766a97808520
Min Zhu <mzhu92@uwo.ca>
2018-11-08 23:43:38 -0500

    makefile for testing

--- Weather/Weather.cpp
+++ Weather/Weather.cpp
@@ -9,26 +9,27 @@ Weather::Weather(){
 vector<string> Weather::getWeather(string city)
 {
     string arguments = city;  //location
-    string filename = "./weatherapi.py ";
-    string command = "python3 ";
-
-    size_t size = 2;
-    vector<string> ret(size);
+    string filename = "./weatherapi.py "; //python script
+    string command = "python3 "; //python shell command
     
     command = command + filename + arguments;
-    system(command.c_str());
+    system(command.c_str()); // execute python script
 
-    ifstream res;
-    string line;
-    res.open("temp_results.txt");
+    //python script is being used to expediate the process of making an API call
+    //with time (hopefully by final submission) code will be refactored into c++
+    //this was communicated to Professor Katchabaw
+
+    size_t size = 2; 
+    vector<string> ret(size); // 0: temperature || 1: condition
+    
+    ifstream res; // response file from API call
+    string line; // read line 
+    res.open("temp_results.txt"); // open response file
     getline(res, line);
     ret[0] = line; //temperature
-    //string temperature = line;
     getline(res, line);
     ret[1] = line; //condition
-    //string conditions = line;
     res.close();
-    //return temperature + " " + conditions;
     return ret;
 
 }
--- Weather/weatherapi.py
+++ Weather/weatherapi.py
@@ -4,21 +4,21 @@ import json
 
 url = "http://api.openweathermap.org/data/2.5/weather?q="
 
-city = sys.argv[1]
-APPID = "d43e3cb9fc1531411e1661d648996d0c"
+city = sys.argv[1] #city name from shell argument
+APPID = "d43e3cb9fc1531411e1661d648996d0c" #API key
 
-url = url + city + "&APPID=" + APPID
-res = requests.get(url)
+url = url + city + "&APPID=" + APPID #API path
+res = requests.get(url) # GET request
 
-json_data = res.json()
+json_data = res.json() # JSON response
 
-temp = json_data["main"]["temp"]
-temp = temp - 273.15
+temp = json_data["main"]["temp"] #current temperature
+temp = temp - 273.15 #convert to Celscius
 temp = int(round(temp))
-conditions = json_data["weather"][0]["main"]
+conditions = json_data["weather"][0]["main"] # get conditions
 
-file  = open("temp_results.txt", "w")
+file  = open("temp_results.txt", "w") # write results to a temp file
 
-file.write(str(temp))
+file.write(str(temp)) # 1st line temp
 file.write("\n")
-file.write(conditions)
+file.write(conditions) # 2nd line conditions
