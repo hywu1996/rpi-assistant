b03e79b1829a19b5bb7ce9114764410258e983ff
Anthony Wong <awong655@uwo.ca>
2018-11-26 16:43:06 -0500

    tokenizer

---
+++ tokenizer.hpp
@@ -0,0 +1,30 @@
+//
+//  tokenizer.hpp
+//  
+//
+//  Created by Anthony Wong on 2018-11-19.
+//
+//
+
+#ifndef tokenizer_hpp
+#define tokenizer_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include "Command.hpp"
+
+class TokenizerClass{
+public:
+    static void tokenize(std::string);
+    
+};
+
+#endif /* tokenizer_hpp */
