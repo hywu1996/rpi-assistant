e6d0e812fb27b765e4f340e5973cae9396355474
Harry Wu <harrywu@Harrys-MacBook-Pro.local>
2018-11-29 18:55:12 -0500

    program.cpp comments and fixed comments in calculator and volume

--- Volume/VolumeControl.cpp
+++ Volume/VolumeControl.cpp
@@ -1,24 +1,23 @@
-//
-//  VolumeControl.cpp
-//  
-//
-//  Created by Harry Wu on 2018-11-27.
-//
-//
+/**
+* @author : Harry Wu
+* @brief  : Set system volume
+* Takes in volume as a string between 1-100
+* and set system volume to that.
+*/
 
 #include "VolumeControl.h"
 
 using namespace std;
 
-/*  @brief Empty constructor
+/*  @brief	: Empty constructor
  */
 VolumeControl::VolumeControl() 
 {
 }
 
-/*  @brief 			Take in volume level between 0 and 100 as string and set system volume to that
- *  @param volume 	string of volume level
- *  @return     	string: success response to setting volume level
+/*  @brief			: Take in volume level between 0 and 100 as string and set system volume to that
+ *  @parameters 	: string volume - string of volume level
+ *  @return     	: string ret - success response to setting volume level
  */
 string VolumeControl::setVolume(string volume)
 {
