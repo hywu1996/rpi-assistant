bbba87105d7206a6040ace351e3d820c80ba6070
Brandon Kong <bkong9@uwo.ca>
2018-11-29 17:41:26 -0500

    Added doxygen comments to SpeechToText, added to README

--- SpeechToText/SpeechToText.hpp
+++ SpeechToText/SpeechToText.hpp
@@ -1,3 +1,10 @@
+/**
+* @author : Brandon Kong (bkong9)
+* @brief  : Handles all the requisite audio recordings and API calls.
+* This utility handles the USB microphone recordings, Google Speech-To-Text API call, and the Google Text-To-Speech API call.
+* Its current iteration uses system calls with gcloud & CURL requests to handle the API calls, and uses the JSON parser functions in order to parse the resulting JSON from Google's Speech-To-Text API for use in functions.
+*/
+
 #ifndef STT_H
 #define STT_H
 
